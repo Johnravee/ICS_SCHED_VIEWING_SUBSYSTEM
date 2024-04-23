7d58804fde3f9131863922b4e1f8db700daad799
Johnravee <johnravemimayn@gmail.com>
2024-04-23 12:42:26 +0800

    ONLY GOD KNOWS WHAT HAPPEN HERE ^^

--- js/schedulepage.js
+++ js/schedulepage.js
@@ -1,15 +1,34 @@
     const hideTable = () => {
             document.querySelector(".result-container").style.display = "none"
+            document.querySelector("tbody").innerHTML = null
         }
 
-   const inputEvent = () => {
+  const convertTime = (time) => {
+    const start = time.split(":");
+    let hours = parseInt(start[0]);
+    let min = parseInt(start[1]);
+
+    const pmOrAm = (hours >= 12) ? "PM" : "AM"
+
+    hours = (hours > 12) ? hours - 12 : hours
+
+    hours = (hours === 0) ? 12 : hours
+
+    min = (min < 10) ? min + "0" : min
+
+    const formattedStartTime = `${hours}:${min} ${pmOrAm}`
+
+    return formattedStartTime;
+}
+
+
+
+   const clickSearch = () => {
     const inputElement = document.getElementById('inputsearch');
-    const value = inputElement.value;
+    let value = inputElement.value;
     
-    if (value.length === 7) {
-        console.log(value);
-        
-        fetch(`/section?section=${value}`, {
+    if (value.length > 0 ){
+            fetch(`/search?search=${value}`, {
             method: "GET",
             headers: {
                 "Content-Type": "application/json"
@@ -20,14 +39,24 @@
                    return res.ok ? res.json() : console.error("Network  response e  rror!");
         })
         .then(datas => {
+            
+        if (datas.length === 0) {
+
+                        document.querySelector(".result-container").style.display = "none";
+                        return;
+                    }
+
+                inputElement.value = null
+
             datas.forEach(data => {
                 const table = document.querySelector("tbody")
                 table.innerHTML += `
                     <tr>
+                        <td>${data.InstructorName}</td>
                         <td>${data.Day}</td>
                         <td>${data.Section}</td>
                         <td>${data.Subject}</td>
-                        <td>${data.TimeDuration}</td>
+                        <td>${convertTime(data.StartTime)} - ${convertTime(data.EndTime)} </td>
                         <td>${data.RoomNumber}</td>
                     <tr>
                 
@@ -37,7 +66,13 @@
             });
         })
         .catch(err => {
-            console.error("Fetch error:", err.message); // Handle fetch errors
-        });
+            console.error("Fetch error:", err.message);
+        })
     }
-};
+  
+        
+    
+    }
+
+
+
--- router/viewsection.js
+++ router/viewsection.js
@@ -2,22 +2,25 @@ const express = require("express");
 const router = express.Router();
 const conn = require("../server/connection");
 
-router.get('/section', async (req, res) => {
+router.get('/search', async (req, res) => {
     try {
-        const  section  = req.query.section; 
- 
+        const  search  = req.query.search; 
         
-        if (!section) {
+     
+        
+        if (!search) {
             return res.status(400).send("Section parameter is missing");
         }
 
-        const queryCommand = `SELECT * FROM schedules WHERE Section LIKE ? `
+        const queryCommand = `SELECT * FROM schedules WHERE Section LIKE ? OR InstructorName LIKE ?`
         
-        conn.query(queryCommand, [`%${section}%`], (err, results) => {
+        conn.query(queryCommand, [`%${search}%`, `%${search}%`], (err, results) => {
             if (err) {
                 console.error("Error query:", err);
                 return res.status(500).send("Error, walang data na nakuha!");
             }
+
+            if(results.length === 0 ) console.error("Walang data");
 
          
             return res.status(200).json(results)
